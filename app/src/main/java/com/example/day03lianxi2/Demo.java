1170e14d70144f3227759625f5549fd56ed91fcf
wupeiyin <1136970917@qq.com>
2017-08-14 20:35:21 +0800

    提交子分支，新增Demo类 2017-8-14 20:35:14

---
+++ app/src/main/java/com/example/day03lianxi2/Demo.java
@@ -0,0 +1,13 @@
+package com.example.day03lianxi2;
+
+import android.util.Log;
+
+/**
+ * Created by 勿忘初心 on 2017/8/14.
+ */
+
+public class Demo {
+    public void get(){
+        Log.e("TAG", "子分支1");
+    }
+}
